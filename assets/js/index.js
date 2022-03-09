cc02376e763eb676e0054d580e0ca18430ac59a2
Anna <akozorkova@gmail.com>
2022-03-09 21:35:39 +0000

    Adjust start button and guidance text after creating a new heading

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,16 @@ start.addEventListener('click', function(){
   let intro = document.querySelector('.intro');
   intro.style.top = '100%';
 });
+
+//creates a new h2 element for game guidence display 
+const newHeading = document.createElement('h2');
+document.getElementById('heading').appendChild(newHeading);
+
+//creates click event listener to activate game after 2000msec delay
+//changes game guidance text and makes start game button invisible
+let begin = document.querySelector('#begin');
+begin.addEventListener('click', function(){
+    $('h2').text('Repeat Now!');
+    $('#begin').hide();
+    setTimeout(nextSequence, 2000);
+});
